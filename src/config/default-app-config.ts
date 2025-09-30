c903688f0db16635fdea26b0c7c63da8b41cda75
Michał Pasternak <michal.dtz@gmail.com>
2025-09-30 22:35:28 +0200

    15 items

--- src/config/default-app-config.ts
+++ src/config/default-app-config.ts
@@ -318,7 +318,7 @@ export class DefaultAppConfig implements AppConfig {
       sortField: 'dc.date.accessioned',
     },
     topLevelCommunityList: {
-      pageSize: 5,
+      pageSize: 15,
     },
     showDiscoverFilters: false,
   };
