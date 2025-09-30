2994ba746198a867adfcff9791dfa786bfce7bae
Michał Pasternak <michal.dtz@gmail.com>
2025-09-30 17:49:04 +0200

    pliterki w cyferkach

--- src/app/shared/utils/short-number.pipe.ts
+++ src/app/shared/utils/short-number.pipe.ts
@@ -1,8 +1,11 @@
 import {
+  Optional,
   Pipe,
   PipeTransform,
 } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
+import { LocaleService } from '../../core/locale/locale.service';
 import { isEmpty } from '../empty.util';
 
 
@@ -11,6 +14,11 @@ import { isEmpty } from '../empty.util';
   standalone: true,
 })
 export class ShortNumberPipe implements PipeTransform {
+
+  constructor(
+    @Optional() private localeService: LocaleService,
+    @Optional() private translateService: TranslateService,
+  ) {}
 
   transform(number: number, args?: any): any {
     // will only work value is a valid number
@@ -23,12 +31,15 @@ export class ShortNumberPipe implements PipeTransform {
     const isNegative = number < 0; // will also work for Negetive numbers
     let key = '';
 
+    // Get locale-specific abbreviations
+    const abbreviations = this.getAbbreviations();
+
     const powers = [
-      { key: 'Q', value: Math.pow(10, 15) },
-      { key: 'T', value: Math.pow(10, 12) },
-      { key: 'B', value: Math.pow(10, 9) },
-      { key: 'M', value: Math.pow(10, 6) },
-      { key: 'K', value: 1000 },
+      { key: abbreviations.quadrillion, value: Math.pow(10, 15) },
+      { key: abbreviations.trillion, value: Math.pow(10, 12) },
+      { key: abbreviations.billion, value: Math.pow(10, 9) },
+      { key: abbreviations.million, value: Math.pow(10, 6) },
+      { key: abbreviations.thousand, value: 1000 },
     ];
 
     for (let i = 0; i < powers.length; i++) {
@@ -41,5 +52,35 @@ export class ShortNumberPipe implements PipeTransform {
       }
     }
     return (isNegative ? '-' : '') + abs + key;
+  }
+
+  private getAbbreviations(): any {
+    // Get the current language from LocaleService or TranslateService
+    let currentLang = null;
+
+    if (this.localeService) {
+      currentLang = this.localeService.getCurrentLanguageCode();
+    } else if (this.translateService) {
+      currentLang = this.translateService.currentLang || this.translateService.defaultLang;
+    }
+
+    // Check if current language is Polish
+    if (currentLang && currentLang.toLowerCase().startsWith('pl')) {
+      return {
+        thousand: ' tys.',
+        million: ' mln',
+        billion: ' mld',
+        trillion: ' bln',
+        quadrillion: ' bld'
+      };
+    }
+    // Default to English abbreviations
+    return {
+      thousand: 'K',
+      million: 'M',
+      billion: 'B',
+      trillion: 'T',
+      quadrillion: 'Q'
+    };
   }
 }
